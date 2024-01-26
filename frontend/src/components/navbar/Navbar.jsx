542eb0ff1e1428306a3cf5947f4263c720559652
Yeeun Choi <yeeunchoi@Yeeuns-MacBook-Pro.local>
2024-01-26 14:56:53 -0800

    menu in navbar added

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './navbar.css'
-
 export const Navbar = () => {
+    const [menu, setMenu] = useState("home")
     return (
-        <div>Navbar</div>
+        <div className='navbar'>
+            <div className='nav-logo'>
+                <img className='logo' src='../logo.png' />
+                <p>Rolls & Rolls</p>
+            </div>
+            <ul className='nav-menu'>
+                <li onClick={() => { setMenu("home") }}>Home{menu === "home" ? <hr /> : <></>}</li>
+                <li onClick={() => { setMenu("menu") }}>Menu{menu === "menu" ? <hr /> : <></>}</li>
+                <li onClick={() => { setMenu("about") }}>About{menu === "about" ? <hr /> : <></>}</li>
+                <li onClick={() => { setMenu("booktable") }}>Book Table{menu === "booktable" ? <hr /> : <></>}</li>
+            </ul>
+            <div className='nav-order'>
+                <button>Order Online</button>
+                {/* <img className='takeout' src='../cart.png' /> */}
+            </div>
+
+        </div>
     )
 }
