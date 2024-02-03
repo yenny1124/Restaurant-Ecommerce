258feef66025820563474da0da5a292f462bc7d3
Yeeun Choi <yeeunchoi@Yeeuns-MacBook-Pro.local>
2024-02-03 03:50:07 -0800

    About page added

--- frontend/src/components/menudisplay/MenuDisplay.jsx
+++ frontend/src/components/menudisplay/MenuDisplay.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import './menudisplay.css'
+import data_items from '../assets/data'
 
 const MenuDisplay = () => {
     return (
         <div className='menudisplay'>
-            <h2>Our Menu</h2>
+            <h1>Our Menu</h1>
+            <hr />
+            <div className='menu-items'>
+                {data_items.map((item)=>{
+                    return 
+                })}
+            </div>
+
         </div>
     )
 }
