6b3f6415fbe8fda7eb17d4304c344b376f1ea4b8
Yeeun Choi <yeeunchoi@Yeeuns-MacBook-Pro.local>
2024-02-22 03:38:02 -0800

    category bar added

--- frontend/src/components/menudisplay/MenuDisplay.jsx
+++ frontend/src/components/menudisplay/MenuDisplay.jsx
@@ -3,7 +3,25 @@ import './menudisplay.css'
 import SushiCard from '../sushicard/SushiCard'
 
 const MenuDisplay = () => {
+    const [categories, setCategories] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState('');
     const [products, setProducts] = useState([]);
+
+    // fetch categories 
+    useEffect(() => {
+        const fetchCategories = async () => {
+            try {
+                const response = await fetch('http://localhost:3003/api/get/categories'); // Adjust URL as needed
+                const data = await response.json();
+                // console.log(data); // to debug
+                setCategories(data);
+            } catch (error) {
+                console.error('Error fetching categories:', error);
+            }
+        };
+
+        fetchCategories();
+    }, []);
 
     useEffect(() => {
         // Function to fetch products from the backend
@@ -12,6 +30,7 @@ const MenuDisplay = () => {
                 // Use Axios or fetch to get the product data
                 const response = await fetch('http://localhost:3003/api/get/products');
                 const data = await response.json();
+                // console.log(data); // to debug
                 setProducts(data);
             } catch (error) {
                 console.error('Error fetching products:', error);
@@ -25,6 +44,17 @@ const MenuDisplay = () => {
         <div className='menudisplay'>
             <h1 className='title'>Our Menu</h1>
             <hr />
+            <div className="category-bar">
+                {categories.map(category => (
+                    <button
+                        key={category._id}
+                        onClick={() => setSelectedCategory(category._id)}
+                        className={selectedCategory === category._id ? 'active-category' : ''}
+                    >
+                        {category.name}
+                    </button>
+                ))}
+            </div>
             <div className='card-wrapper'>
                 {products.map((product) => (
                     <SushiCard key={product._id} product={product} />
